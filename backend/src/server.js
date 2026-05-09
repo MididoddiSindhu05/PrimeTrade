bb70ec5493c12d7766d8f043684e0c19d92db02c
Mididoddi Sindhu <mididoddisindhu59@gmail.com>
2026-05-09 14:55:15 +0530

    Change sequelize sync to force: true to fix SQLite alter issues

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 async function start() {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: true });
     app.listen(PORT, () => {
       console.log(`Backend running on http://localhost:${PORT}`);
     });
